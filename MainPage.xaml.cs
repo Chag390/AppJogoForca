5a6d941de490a8318ec129a2d58f3066c8151774
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2023-11-21 03:48:53 -0300

    Aula 06 - Seção 07 - Programação Inicial -Escolher palavra

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,14 +1,28 @@
-﻿namespace AppJogoForca;
+﻿using AppJogoForca.Models;
+using AppJogoForca.Repositories;
+
+namespace AppJogoForca;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	private Word _word;
 	public MainPage()
 	{
 		InitializeComponent();
+
+		var repository = new WordRepositories();
+		_word = repository.GetRandomWord();
+
+		LblTips.Text = _word.Tips;
+		LblText.Text = new string('_', _word.Text.Length);
+
 	}
 
+	private void OnButtonClicked(object sender, EventArgs e)
+	{ 
+		String letter = ((Button)sender).Text;
 	
+	}
+
 }
 

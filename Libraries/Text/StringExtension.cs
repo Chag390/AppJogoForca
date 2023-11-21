6332ea7c56d51c113e433339cad3db21b5827a3e
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2023-11-21 04:13:07 -0300

    sasa

--- Libraries/Text/StringExtension.cs
+++ Libraries/Text/StringExtension.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AppJogoForca.Libraries.Text
 {
-    internal class StringExtension
+    public static class StringExtension
     {
+
+        public static List<int> AllIndexesOf(this string str, string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
+
     }
 }
